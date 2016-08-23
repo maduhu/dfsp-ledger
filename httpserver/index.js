235fd9d565d9695e2455ddbbd17ee2e72d8de7ad
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-08-23 18:20:52 +0300

    refactoring

--- httpserver/index.js
+++ httpserver/index.js
@@ -7,6 +7,7 @@ module.exports = {
   port: 8014,
   bundle: 'ledger',
   dist: path.resolve(__dirname, '../dist'),
+  imports: ['ledger.start'],
   routes: {
     rpc: {
       method: '*',
@@ -15,42 +16,5 @@ module.exports = {
         auth: false
       }
     }
-  },
-  start: function () {
-    var port = this
-
-    function rest (request, reply, method) {
-      // Used in case header is "Content-Type: text/plain"
-      if (typeof request.payload === 'string') {
-        request.payload = {
-          params: {
-            plainText: request.payload
-          }
-        }
-      }
-      Object.assign(request.payload.params, request.params)
-      request.params.method = method
-      return port.handler(request, reply)
-    }
-
-    var routes = [
-      { rpc: 'ledger.account.get', path: '/ledger/accounts/{accountNumber}', method: 'get' },
-      { rpc: 'ledger.account.edit', path: '/ledger/accounts/{accountNumber}', method: 'put' },
-      { rpc: 'ledger.connectors.get', path: '/ledger/connectors', method: 'get' },
-      { rpc: 'ledger.transfer.hold', path: '/ledger/transfers/{id}', method: 'put' },
-      { rpc: 'ledger.transfer.get', path: '/ledger/transfers/{id}', method: 'get' },
-      { rpc: 'ledger.transfer.getFulfillment', path: '/ledger/transfers/{id}/fulfillment', method: 'get' },
-      { rpc: 'ledger.transfer.getState', path: '/ledger/transfers/{id}/state', method: 'get' },
-      { rpc: 'ledger.transfer.execute', path: '/ledger/transfers/{transferId}/fulfillment', method: 'put' }
-    ].map((route) => {
-      return {
-        method: route.method,
-        path: route.path,
-        handler: (request, reply) => rest(request, reply, route.rpc),
-        config: { auth: false }
-      }
-    })
-
-    this.registerRequestHandler(routes)
   }
 }
--- service/ledger/index.js
+++ service/ledger/index.js
@@ -5,6 +5,43 @@ module.exports = {
     path: path.join(__dirname, 'schema'),
     linkSP: true
   }],
+  start: function () {
+    if (!this.registerRequestHandler) {
+      return
+    }
+    var port = this
+
+    function rest (request, reply, method) {
+      // Used in case header is "Content-Type: text/plain"
+      if (typeof request.payload === 'string') {
+        request.payload = Object.assign({plainText: request.payload}, request.params)
+      } else {
+        Object.assign(request.payload.params, request.params)
+      }
+      request.params.method = method
+      return port.handler(request, reply)
+    }
+
+    var routes = [
+      { rpc: 'ledger.account.get', path: '/ledger/accounts/{accountNumber}', method: 'get' },
+      { rpc: 'ledger.account.edit', path: '/ledger/accounts/{accountNumber}', method: 'put' },
+      { rpc: 'ledger.connectors.get', path: '/ledger/connectors', method: 'get' },
+      { rpc: 'ledger.transfer.hold', path: '/ledger/transfers/{id}', method: 'put' },
+      { rpc: 'ledger.transfer.get', path: '/ledger/transfers/{id}', method: 'get' },
+      { rpc: 'ledger.transfer.getFulfillment', path: '/ledger/transfers/{id}/fulfillment', method: 'get' },
+      { rpc: 'ledger.transfer.getState', path: '/ledger/transfers/{id}/state', method: 'get' },
+      { rpc: 'ledger.transfer.execute', path: '/ledger/transfers/{transferId}/fulfillment', method: 'put' }
+    ].map((route) => {
+      return {
+        method: route.method,
+        path: route.path,
+        handler: (request, reply) => rest(request, reply, route.rpc),
+        config: { auth: false }
+      }
+    })
+
+    this.registerRequestHandler(routes)
+  },
   'transfer.hold.request.send': function (msg, $meta) {
     if (!Array.isArray(msg.debits) || !Array.isArray(msg.credits) || msg.debits.length !== 1 || msg.credits.length !== 1) {
       // TODO: throw invalid params exception
@@ -19,14 +56,15 @@ module.exports = {
 
     delete msg.debits
     delete msg.credits
+    delete msg.ledger
 
     return msg
   },
   'transfer.execute.request.send': function (msg, $meta) {
-    msg.fulfillment = msg.params.plainText
-    msg.condition = cc.fulfillmentToCondition(msg.params.plainText)
+    msg.fulfillment = msg.plainText
+    msg.condition = cc.fulfillmentToCondition(msg.plainText)
 
-    delete msg.params.plainText
+    delete msg.plainText
     return msg
   },
   'transfer.get.response.receive': function (msg, $meta) {
