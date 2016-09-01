2d8d984e938bf7577d45a4be51d92b25c96bede7
pavel.georgiev <pavel.georgiev@softwaregroup-bg.com>
2016-09-01 16:29:47 +0300

    add new procedure

---
+++ service/ledger/schema/750$ledger.transfer.getFulfillment.sql
@@ -0,0 +1,17 @@
+﻿CREATE OR REPLACE FUNCTION ledger."transfer.getFulfillment"("@uuid" character varying(100)
+)
+RETURNS character varying(100)
+
+AS
+$BODY$
+
+  declare   "@fulfillment" character varying(100)=(select fulfillment from ledger.transfer where uuid="@uuid" );
+
+
+BEGIN
+	 return "@fulfillment";
+
+ END
+$BODY$
+LANGUAGE plpgsql
+	
