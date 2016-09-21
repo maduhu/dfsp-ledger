53ab3853e0279acc9797c99a8d241ff6e775e207
pavel.georgiev <pavel.georgiev@softwaregroup-bg.com>
2016-09-21 17:46:07 +0300

    add and change procedure

---
+++ service/ledger/schema/750$ledger.account.edit.sql
@@ -0,0 +1,64 @@
+﻿CREATE OR REPLACE FUNCTION ledger."account.edit"("@accountNumber" character varying(100)
+,"@debit" "numeric"(19,2)
+,"@credit" "numeric"(19,2)
+,"@name" character varying(20)
+,"@displayName" character varying(100)
+,"@accountTypeId" INT
+,"@currencyId" char(3)
+
+)
+RETURNS
+    TABLE("accountNumber" character varying(100)
+    , "balance" "numeric"(19,2)
+    , "isActive" bit
+   )
+AS
+$BODY$
+  declare
+  "@accountId" BIGINT:=(SELECT nextval('ledger."account_accountId_seq"'));
+BEGIN
+INSERT INTO
+  ledger.account
+(
+  "accountId",
+  name,
+  "displayName",
+  "accountNumber",
+  credit,
+  debit,
+  "accountTypeId",
+  "isActive",
+  "parentId",
+  "creationDate",
+  "currencyId"
+)
+VALUES (
+  "@accountId",
+  "@name",
+ "@displayName",
+  "@accountNumber",
+  "@credit",
+  "@debit",
+  "@accountTypeId",
+  CAST(1 as BIT),
+  NULL,
+  now(),
+  "@currencyId"
+);
+
+
+
+  return QUERY
+      SELECT
+        a."accountNumber",
+        a.credit-a.debit,
+        a."isActive"
+    FROM
+        ledger."account" a
+    WHERE
+        a."accountId"="@accountId";
+
+ END
+$BODY$
+LANGUAGE plpgsql
+	
--- service/ledger/schema/750$ledger.account.get.sql
+++ service/ledger/schema/750$ledger.account.get.sql
@@ -1,15 +1,20 @@
-CREATE OR REPLACE FUNCTION ledger."account.get"("accountNumber" character varying)
-RETURNS
-    TABLE("displayName" character varying(100), "accountNumber" character varying(20), "currencyId" character(3))
-AS
-$BODY$
+CREATE OR REPLACE FUNCTION ledger."account.get" (
+  "accountNumber" varchar
+)
+RETURNS TABLE (
+  "accountNumber" varchar,
+  balance numeric(19,2),
+  "isDisable" bit
+) AS
+$body$
     SELECT
-        a."displayName",
         a."accountNumber",
-        a."currencyId"
+        a.credit-a.debit,
+        case when a."isActive"=CAST(1 as bit) then CAST(1 as bit) else CAST(0 as bit) end
     FROM
         ledger."account" a
     WHERE
         a."accountNumber"="accountNumber"
-$BODY$
-LANGUAGE SQL
+$body$
+LANGUAGE 'sql'
+ 
