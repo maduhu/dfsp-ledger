4bde7dcf5fb01ce7ae13eb26334718fb0fa31044
Dimitar Timofeev <dimitar.timofeev@softwaregroup-bg.com>
2016-09-26 13:52:50 +0300

    feat: initial implementation for ledger.getServerMeta method

--- service/ledger/index.js
+++ service/ledger/index.js
@@ -253,6 +253,44 @@ module.exports = {
           }
         },
         method: 'put'
+      },
+      {
+        rpc: 'ledger.getServerMeta',
+        path: '/ledger',
+        reply: (reply, response, $meta) => {
+          return reply(response, {'content-type': 'text/plain'}, 200)
+        },
+        config: {
+          description: 'Get Server Metadata',
+          notes: 'Receive information about the ILP Ledger Adapter.',
+          tags: ['api'],
+          plugins: {
+            'hapi-swagger': {
+              responses: {
+                '200': {
+                  description: 'Transfer was executed successfully.',
+                  schema: joi.object({
+                    currency_code: joi.string().allow(null),
+                    currency_symbol: joi.string().allow(null),
+                    condition_sign_public_key: joi.string().allow(null),
+                    notification_sign_public_key: joi.string().allow(null),
+                    urls: joi.object({
+                      transfer: joi.string(),
+                      transfer_fulfillment: joi.string(),
+                      transfer_state: joi.string(),
+                      accounts: joi.string(),
+                      account: joi.string(),
+                      subscription: joi.string()
+                    }),
+                    precision: joi.number().integer(),
+                    scale: joi.number().integer()
+                  })
+                }
+              }
+            }
+          }
+        },
+        method: 'get'
       }
     ].map((route) => {
       return {
@@ -381,6 +419,24 @@ module.exports = {
         'prepared_at': transfer.preparedAt,
         'executed_at': transfer.executedAt
       }
+    }
+  },
+  'getServerMeta': function (msg, $meta) {
+    return {
+      currency_code: null,
+      currency_symbol: null,
+      condition_sign_public_key: '',
+      notification_sign_public_key: '',
+      urls: {
+        transfer: ledgerAddress + '/transfers/:id',
+        transfer_fulfillment: ledgerAddress + '/transfers/:id/fulfillment',
+        transfer_state: ledgerAddress + ledgerAddress + '/transfers/:id/state',
+        accounts: ledgerAddress + '/accounts',
+        account: ledgerAddress + '/accounts/:name',
+        subscription: ledgerAddress + '/subscriptions/:id'
+      },
+      precision: 10,
+      scale: 2
     }
   }
 }
