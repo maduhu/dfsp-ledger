0970682f569311446e179a7f7c127d4c0428f330
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-12-20 14:04:14 +0200

    feat: add ledger.ministatement.get db function added

---
+++ service/ledger/schema/750$ledger.ministatement.get.sql
@@ -0,0 +1,31 @@
+﻿CREATE OR REPLACE FUNCTION ledger."transfer.get"(
+	"@accountId" character varying(100)
+) RETURNS TABLE (
+	"name" varchar(100),
+	"amount" varchar(20),
+	"date" timestamp without time zone
+) AS
+$BODY$
+	SELECT
+		CASE WHEN "debitAccountId" = "@accountId"
+			THEN CAST("creditMemo"->'ilp_header'->'data'->'data'->>'memo' AS json)->>'creditName'
+			ELSE CAST("creditMemo"->'ilp_header'->'data'->'data'->>'memo' AS json)->>'debitName'
+		END AS "name",
+		CONCAT(CASE WHEN "debitAccountId" = "@accountId" THEN '-' ELSE '' END, CAST("amount" AS varchar)) AS "amount",
+		"transferDate" AS "date"
+	FROM
+		ledger.transfer
+	WHERE
+		"debitAccountId" = "@accountId" OR "creditAccountId" = "@accountId"
+	UNION ALL
+	SELECT
+		'fee' AS "name",
+		CONCAT('-', CAST("amount" AS varchar)) AS "amount",
+		"transferDate" AS "date"
+	FROM
+		ledger.fee
+	WHERE
+		"debitAccountId" = "@accountId"
+	ORDER BY "date" DESC
+$BODY$
+LANGUAGE SQL
