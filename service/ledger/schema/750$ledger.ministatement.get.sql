5782a452977c08130df515ed901dc921a36adedc
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-12-20 14:42:44 +0200

    fix: fix ledger.ministatement.get

--- service/ledger/schema/750$ledger.ministatement.get.sql
+++ service/ledger/schema/750$ledger.ministatement.get.sql
@@ -1,31 +1,35 @@
 ﻿CREATE OR REPLACE FUNCTION ledger."ministatement.get"(
-	"@accountId" character varying(100)
+	"@accountNumber" varchar(50)
 ) RETURNS TABLE (
-	"name" varchar(100),
-	"amount" varchar(20),
+	"name" varchar(50),
+	"amount" varchar(50),
 	"date" timestamp without time zone
 ) AS
 $BODY$
-	SELECT
-		CASE WHEN "debitAccountId" = "@accountId"
-			THEN CAST("creditMemo"->'ilp_header'->'data'->'data'->>'memo' AS json)->>'creditName'
-			ELSE CAST("creditMemo"->'ilp_header'->'data'->'data'->>'memo' AS json)->>'debitName'
-		END AS "name",
-		CONCAT(CASE WHEN "debitAccountId" = "@accountId" THEN '-' ELSE '' END, CAST("amount" AS varchar)) AS "amount",
-		"transferDate" AS "date"
-	FROM
-		ledger.transfer
-	WHERE
-		"debitAccountId" = "@accountId" OR "creditAccountId" = "@accountId"
-	UNION ALL
-	SELECT
-		'fee' AS "name",
-		CONCAT('-', CAST("amount" AS varchar)) AS "amount",
-		"transferDate" AS "date"
-	FROM
-		ledger.fee
-	WHERE
-		"debitAccountId" = "@accountId"
-	ORDER BY "date" DESC
+	DECLARE "@accountId" bigint:=(SELECT "accountId" FROM ledger.account WHERE "accountNumber" = "@accountNumber");
+	BEGIN
+		RETURN query
+		SELECT
+			CASE WHEN "debitAccountId" = "@accountId"
+				THEN CAST("creditMemo"->'ilp_header'->'data'->'data'->>'memo' AS json)->>'creditName'
+				ELSE CAST("creditMemo"->'ilp_header'->'data'->'data'->>'memo' AS json)->>'debitName'
+			END AS "name",
+			CONCAT(CASE WHEN "debitAccountId" = "@accountId" THEN '-' ELSE '' END, CAST("amount" AS varchar)) AS "amount",
+			"transferDate" AS "date"
+		FROM
+			ledger.transfer
+		WHERE
+			"debitAccountId" = "@accountId" OR "creditAccountId" = "@accountId"
+		UNION ALL
+		SELECT
+			'fee' AS "name",
+			CONCAT('-', CAST("amount" AS varchar)) AS "amount",
+			"transferDate" AS "date"
+		FROM
+			ledger.fee
+		WHERE
+			"debitAccountId" = "@accountId"
+		ORDER BY "date" DESC;
+	END
 $BODY$
-LANGUAGE SQL
+LANGUAGE plpgsql
