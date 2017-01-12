9fe5905b1ecec40d56f54116d79f5fb580f77d75
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-01-12 16:06:54 +0200

    refactor: return ledger name for ledger fetch query

--- service/ledger/schema/750$ledger.account.fetch.sql
+++ service/ledger/schema/750$ledger.account.fetch.sql
@@ -3,6 +3,7 @@ CREATE OR REPLACE FUNCTION ledger."account.fetch" (
 )
 RETURNS TABLE (
   "accountNumber" varchar,
+  "name" varchar,
   "balance" numeric(19,2),
   "currencyCode" character(3),
   "currencySymbol" varchar(10),
@@ -11,6 +12,7 @@ RETURNS TABLE (
 $body$
     SELECT
         a."accountNumber" AS "accountNumber",
+        a."name" AS "name",
         a.credit-a.debit AS "balance",
         a."currencyId" AS "currencyCode",
         c."symbol" AS "currencySymbol",
