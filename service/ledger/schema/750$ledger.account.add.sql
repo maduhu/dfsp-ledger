390455b43e1c08a8d574f76bccfbd7d65e07de09
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-03-15 16:16:43 +0200

    fix: throw proper exception if account number exists

--- service/ledger/schema/750$ledger.account.add.sql
+++ service/ledger/schema/750$ledger.account.add.sql
@@ -1,10 +1,10 @@
 ﻿CREATE OR REPLACE FUNCTION ledger."account.add"(
-  "@accountNumber" character varying(100),
-  "@debit" "numeric"(19,2),
-  "@credit" "numeric"(19,2),
-  "@name" character varying(20),
-  "@accountTypeId" INT,
-  "@currencyId" char(3)
+    "@accountNumber" character varying(100),
+    "@debit" "numeric"(19,2),
+    "@credit" "numeric"(19,2),
+    "@name" character varying(20),
+    "@accountTypeId" INT,
+    "@currencyId" char(3)
 )
 RETURNS TABLE(
     "accountNumber" character varying(100),
@@ -14,45 +14,50 @@ RETURNS TABLE(
 )
 AS
 $BODY$
-  declare "@accountId" BIGINT:=(SELECT nextval('ledger."account_accountId_seq"'));
+    declare "@accountId" BIGINT:=(SELECT nextval('ledger."account_accountId_seq"'));
 BEGIN
+
+IF EXISTS (SELECT 1 FROM ledger.account a WHERE a."accountNumber" = COALESCE("@accountNumber", "@name")) THEN
+    RAISE EXCEPTION 'ledger.account.add.exists';
+END IF;
+
 INSERT INTO
   ledger.account
-(
-  "accountId",
-  "name",
-  "accountNumber",
-  "credit",
-  "debit",
-  "accountTypeId",
-  "isDisabled",
-  "parentId",
-  "creationDate",
-  "currencyId"
-)
-VALUES (
-  "@accountId",
-  "@name",
-  COALESCE("@accountNumber", "@name"),
-  "@credit",
-  "@debit",
-  "@accountTypeId",
-  FALSE,
-  NULL,
-  now(),
-  "@currencyId"
-);
+  (
+      "accountId",
+      "name",
+      "accountNumber",
+      "credit",
+      "debit",
+      "accountTypeId",
+      "isDisabled",
+      "parentId",
+      "creationDate",
+      "currencyId"
+  )
+  VALUES (
+      "@accountId",
+      "@name",
+      COALESCE("@accountNumber", "@name"),
+      "@credit",
+      "@debit",
+      "@accountTypeId",
+      FALSE,
+      NULL,
+      now(),
+      "@currencyId"
+  );
 
-  return QUERY
-      SELECT
-        a."accountNumber",
-        a.credit-a.debit,
-        a."currencyId",
-        a."isDisabled"
-    FROM
-        ledger."account" a
-    WHERE
-        a."accountId"="@accountId";
+  RETURN QUERY
+  SELECT
+      a."accountNumber",
+      a.credit-a.debit,
+      a."currencyId",
+      a."isDisabled"
+  FROM
+      ledger."account" a
+  WHERE
+      a."accountId"="@accountId";
 
  END
 $BODY$
