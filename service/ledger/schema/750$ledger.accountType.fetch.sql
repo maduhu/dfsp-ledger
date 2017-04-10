66faeab81425f5e59a4815da1493a895e55c2fa2
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-04-10 13:32:21 +0300

    fix: add commission account type and ledger.accountType.fetch db function

---
+++ service/ledger/schema/750$ledger.accountType.fetch.sql
@@ -0,0 +1,21 @@
+﻿CREATE OR REPLACE FUNCTION ledger."accountType.fetch"(
+
+)
+RETURNS TABLE(
+    "accountTypeId" int,
+    "name" varchar(25),
+    "code" varchar(20)
+)
+AS
+$BODY$
+  BEGIN
+    RETURN QUERY
+      SELECT
+          at."accountTypeId",
+          at."name",
+          at."code"
+      FROM
+          ledger."accountType" at;
+  END
+$BODY$
+LANGUAGE plpgsql
